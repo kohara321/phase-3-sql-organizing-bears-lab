2c92c9f7b39100ddfb34385fad6c6c938dcd3b88
Kaegan O'Hara <kohara321@gmail.com>
2022-06-30 12:25:11 -0600

    commit tree 2c9d4e16c978e64a1f14be5279d3882304309ebc

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Mr.Chocolate", 15, "M", "Black", "Sweet", TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Rowdy",13, "M", "Brown","Rowdy", TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Tabitha",20, "F", "Black","Sassy", TRUE );
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Sergeant Brown",18, "M", "Brown", "Angry", FALSE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Melissa",9, "F", "Black", "Happy", TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Grinch",15, "M", "Brown", "Angry", TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES ("Wendy",5, "F", "Black", "Happy", TRUE);
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES (NULL,NULL, "M", "Brown", NULL, FALSE);
